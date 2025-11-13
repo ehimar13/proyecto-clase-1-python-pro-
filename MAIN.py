a8d19110d1816b21297658e772708452752413a2
Ehimar Vargas <ehimar13@gmail.com>
2025-11-12 20:50:53 -0500

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,1 +1,14 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "PEPE": "La masco del colegio de Leandro"
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
 
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    # ¿Qué debemos hacer si se encuentra la palabra?
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
+    print("esa palabra aun no esta definida")
+    
